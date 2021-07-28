c65439a61b01a7656804be621b6c33e76a4a57d5
Sonali Singh <sonali122000@gmail.com>
2021-07-28 17:01:53 +0530

    add tweet box component

--- src/components/TweetBox.js
+++ src/components/TweetBox.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import { Avatar, Button } from "@material-ui/core";
 
 const TweetBox = () => {
+
+    const [tweetMessage, setTweetMessage] = useState("");
+    const [tweetImage, setTweetImage] = useState("");
+
     return (
-        <div>
-            Tweets
-        </div>
+        <div className="tweetBox">
+        <form>
+          <div className="tweetBox__input">
+            <Avatar src="https://image.pngaaa.com/408/81408-middle.png" />
+            <input
+              onChange={(e) => setTweetMessage(e.target.value)}
+              value={tweetMessage}
+              placeholder="What's happening?"
+              type="text"
+            />
+          </div>
+          <input
+            value={tweetImage}
+            onChange={(e) => setTweetImage(e.target.value)}
+            className="tweetBox__imageInput"
+            placeholder="Optional: Enter image URL"
+            type="text"
+          />
+  
+          <Button
+            type="submit"
+            className="tweetBox__tweetButton"
+          >
+            Tweet
+          </Button>
+        </form>
+      </div>
     )
 }
 
